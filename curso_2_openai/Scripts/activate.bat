f4d1781fb8d4292f72a2904b30c387bd6515c8fd
engluizSalles <simoesluiz998@gmail.com>
2024-06-20 12:51:04 -0300

    Commit do código com os requirements revisados

--- curso_2_openai/Scripts/activate.bat
+++ curso_2_openai/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=E:\Projetos\iart_verum\curso_2_openai
+set VIRTUAL_ENV=E:\IART\IART\curso_2_openai
 
 if not defined PROMPT set PROMPT=$P$G
 
